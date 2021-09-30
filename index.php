d41dad46ad20aa74c5629e72da9a929137617c77
silkslime <silkslime@yandex.ru>
2021-10-01 01:17:05 +0300

    Moved websockets files to new folder, improved main page

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
 </head>
+
 <body>
-    Welcome! You are in the main page!
+    <h1>Welcome! You are in the main page!</h1>
+    <h2>Avalible APIs:</h2>
+    <ul>
+        <li>api/ads</li>
+        <ul>
+            <li><a href="./api/ads/getbanner.php">api/ads/getbanner.php</a></li>
+        </ul>
+        <li>api/canvas</li>
+        <ul>
+            <li><a href="./api/canvas/points.php">api/canvas/points.php</li>
+            <li><a href="./api/canvas/setpoint.php">api/canvas/setpoint.php?x=228&y=1337</li>
+        </ul>
+        <li>api/chat</li>
+        <ul>
+            <li><a href="./api/chat/getmessages.php">api/chat/getmessages.php</li>
+            <li><a href="./api/chat/getnew.php">api/chat/getnew.php?last=2021-09-01 15:28:55</li>
+            <li><a href="./api/chat/sendmessage.php">api/chat/sendmessage.php?author=SilkSlime&text=Hello!</li>
+        </ul>
+        <li>/weather</li>
+        <ul>
+            <li><a href="./api/weather/feel.php">api/weather/feel.php</li>
+            <li><a href="./api/weather/humidity.php">api/weather/humidity.php</li>
+            <li><a href="./api/weather/img.php">api/weather/img.php</li>
+            <li><a href="./api/weather/json.php">api/weather/json.php</li>
+            <li><a href="./api/weather/temp.php">api/weather/temp.php</li>
+            <li><a href="./api/weather/wind.php">api/weather/wind.php</li>
+        </ul>
+    </ul>
 </body>
+
 </html>
